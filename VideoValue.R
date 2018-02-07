32c0e6350d3ffcd7274d7aa9b03245244bc66e9b
ClausBoHansen <sbo@trolling.dk>
2018-02-07 18:46:59 +0100

    Update video value function

--- VideoValue.R
+++ VideoValue.R
@@ -6,6 +6,11 @@ library(lubridate)
 
 source("Config.R")
 
+sigmoid <- function(relslope, age) {
+      # Calculates correction factor for estimate of future watch minutes
+      min(1, 1 + (standardLifetime/(standardLifetime + 3 * age)) * (relslope / (1 + abs(relslope))))
+}
+
 get.video.value <- function(estimateForVideoId, estimationdate = latestdate, minobs = minimumObservations) {
 
       videoTraffic <- VIDEOxAGE[which(videoId == estimateForVideoId & day < estimationdate)]
@@ -13,10 +18,12 @@ get.video.value <- function(estimateForVideoId, estimationdate = latestdate, min
       
       pastMinutes <- as.numeric(NULL)
       futureMinutes <- as.numeric(NULL)
-#      m <- NULL
+      m <- NULL
       slope <- NULL
       intercept <- NULL
       estimationAverage <- NULL
+      relativeslope <- NULL
+      estimatedRemainingLifetime <- NULL
       
       # If there are sufficient observations to estimate value
       if (observations >= minobs) {
@@ -33,31 +40,42 @@ get.video.value <- function(estimateForVideoId, estimationdate = latestdate, min
                   
                   # Fit linear regression model to estimationTraffic            
                   model = lm(estimatedMinutesWatched ~ age, data = estimationTraffic)
-#                  model = lm(estimatedMinutesWatched ~ age, data = videoTraffic)
                   intercept <- model$coefficients[1]
                   slope <- model$coefficients[2]
                   estimationAverage <- mean(estimationTraffic$estimatedMinutesWatched)
+
+                  # Calculate relative slope
+                  relativeslope <- as.numeric(minobs*slope/estimationAverage)
                   
-                  # Use area under traffic curve if slope is below limit and level over threshold
-#                  if ((-estimationAverage/slope < 2 * constantTrafficDuration) & slope < 0) {
-                  if ((-estimationAverage/slope < 2 * constantTrafficDuration) & slope < 0 & estimationAverage > minimumMinsPerDay) {
-                              futureMinutes <-  as.numeric(-(estimationAverage^2) / (2*slope))
-                        m <- "grad"
-                  }
-                  # Slope is above limit, use constant traffic model
-                  else {
-                        futureMinutes <-  mean(estimationTraffic$estimatedMinutesWatch) * constantTrafficDuration
-                        m <- "const"
-                  }
+                  # Calculate estimated remaining lifetime
+                  initialAverage <- mean(videoTraffic[1:minobs,]$estimatedMinutesWatched)
+                  estimationAverageDate <- mean(estimationTraffic$day)
+                  initialAverageDate <- mean(videoTraffic[1:minobs,]$day)
+                  
+                  # Calculate difference in days between initial average and estimation average
+                  estimationAge <- as.numeric(estimationAverageDate-initialAverageDate)
+                  
+                  # Age of video, since first view
+                  age <- as.numeric(date(estimationdate) - date(videoTraffic[which.min(day)]$day))
+
+                  # Estimated remaining lifetime                  
+                  estimatedRemainingLifetime <- min(maximumLifetime - age, max(standardLifetime - age, estimationAge * (estimationAverage/initialAverage)))
+
+                  # Estimated future minutes
+                  futureMinutes <- estimationAverage * estimatedRemainingLifetime * sigmoid(relativeslope, age)
+                  
+                  m <- "sigmoid"
+
             }
             # Not enough data to estimate future minutes
             else {
                   futureMinutes <- 0
                   m <- "timeout"
             }
-            
+
+            # Total estimated value of video            
             estimatedValue <- (pastMinutes + futureMinutes) * minuteValue
-            
+
       }
 
       # If there are not enough observations to estimate value
@@ -66,12 +84,12 @@ get.video.value <- function(estimateForVideoId, estimationdate = latestdate, min
             pastMinutes <- as.numeric(NA)
             futureMinutes <- as.numeric(NA)
             m <- "fewobs"
-#            dayMins <- as.numeric(NA)
       }
 
-#      estimatedValue
-#      list(estvalue = estimatedValue, pastMinutes, futureMinutes, m, estimationAverage, slope)
+      # Minutes watch on estimation date
       dayMins <- VIDEOxAGE[which(videoId == estimateForVideoId & day == estimationdate)]$estimatedMinutesWatched
-      if (is.null(dayMins)) dayMins <- 0
-      list(estvalue = estimatedValue, pastmin = pastMinutes, futuremin = futureMinutes, method = m, daymins = dayMins)
+      if (length(dayMins) == 0) dayMins <- 0
+      
+      # Return result
+      list(estvalue = estimatedValue, pastmin = pastMinutes, futuremin = futureMinutes, method = m, daymins = dayMins, relslope = relativeslope, restlife = estimatedRemainingLifetime)
 }
