2901012a894a70c8425017efac74cd7d68d8f99a
ClausBoHansen <sbo@trolling.dk>
2018-02-08 12:41:58 +0100

    Added SOURCExCHANNEL table

--- ProcessData.R
+++ ProcessData.R
@@ -16,16 +16,18 @@ load(paste(datadir, rawfile, sep = ""))
 processedtables <- append(datatables, "channels")
 processedtables <- append(processedtables, "minuteValue")
 
-
+########################################################################
 # views and watch time by channel and day
 CHANNELxDAY <- merge(videos, videostats, by = "videoId") %>%
       merge(channels, by = "channelId") %>%
       as.data.table() %>%
       group_by(channelName, day) %>%
       summarise(views = sum(views), minutes = sum(estimatedMinutesWatched), subscribersChange = sum(subscribersGained) - sum(subscribersLost))
+
 processedtables <- append(processedtables, "CHANNELxDAY")
 
 
+########################################################################
 # Calculate subscribers
 SUBSCRIBERSxDAY <- merge(channels, subscribers, by = "channelId") %>% as.data.table()
 SUBSCRIBERSxDAY <- SUBSCRIBERSxDAY[order(channelName, day)]
@@ -43,17 +45,21 @@ for (i in 1:nrow(SUBSCRIBERSxDAY)) {
       previoussum <- newsum
       previouschannel <- SUBSCRIBERSxDAY[i, "channelName"]
 }
+
 processedtables <- append(processedtables, "SUBSCRIBERSxDAY")
 
 
+########################################################################
 # Views and minutes by video and age of video
 VIDEOxAGE <- merge(videostats, videos, by = "videoId") %>%
       subset(select = c(videoId, day, publishedAt,views, estimatedMinutesWatched, averageViewPercentage, channelTitle))
 # Calculate age in days
 VIDEOxAGE$age <- as_date(VIDEOxAGE$day) - as.Date(VIDEOxAGE$publishedAt)
+
 processedtables <- append(processedtables, "VIDEOxAGE")
 
 
+########################################################################
 # Estimate value of videos
 VIDEOxVALUE <- videos %>% subset(select = -c(position))
 VIDEOxVALUE$value <- as.numeric(NA)
@@ -68,5 +74,18 @@ for (i in 1:nrow(VIDEOxVALUE)) {
 processedtables <- append(processedtables, "VIDEOxVALUE")
 
 
+########################################################################
+# Calculate value of past traffic by source and channel
+SOURCExCHANNEL <- merge(trafficsources, videos, by.x = "video", by.y = "videoId") %>%
+      merge(channels, by = "channelId") %>%
+      subset(select = c(day, insightTrafficSourceType, estimatedMinutesWatched, channelName)) %>%
+      group_by(channelName,insightTrafficSourceType, day) %>%
+      summarise(estimatedMinutesWatched = sum(estimatedMinutesWatched)) %>%
+      filter(estimatedMinutesWatched > 0)
+      
+processedtables <- append(processedtables, "SOURCExCHANNEL")
+
+
+########################################################################
 # Save all tables
 save(list = processedtables, file = paste(datadir, processedfile, sep = ""))
